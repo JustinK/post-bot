c26c59a478eb9a40be72e4e2a79fe6006d344f97
Justin Kelly <justinhobbskelly@gmail.com>
2015-06-18 22:14:00 -0500

    added scheduler and posts view

--- backend/api/controllers/PostController.js
+++ backend/api/controllers/PostController.js
@@ -13,20 +13,37 @@ module.exports = {
 			console.log(user);
 			
 			var message = req.body.message;
+			var datetime = req.body.datetime;
+			var owner = req.userId;
 			
-			var T = new Twit({
-			    consumer_key: config.TWITTER_KEY,
-				consumer_secret: config.TWITTER_SECRET,
-				access_token: user.twitterToken,
-				access_token_secret: user.twitterSecret
+			Post.create({
+				message: message,
+				datetime: datetime,
+				owner: owner
+			}).exec(function(err, post){
+				console.log('working', post, err);
 			});
+			
+//			var T = new Twit({
+//			    consumer_key: config.TWITTER_KEY,
+//				consumer_secret: config.TWITTER_SECRET,
+//				access_token: user.twitterToken,
+//				access_token_secret: user.twitterSecret
+//			});
+//
+//			T.post('statuses/update', {
+//				status: message
+//			}, function(err, data, response) {
+//			  console.log(data, err)
+//			});
 
-			T.post('statuses/update', {
-				status: message
-			}, function(err, data, response) {
-			  console.log(data, err)
-			});
 		});
 
+	},
+	
+	myPosts: function (req, res){
+		Post.find({owner:req.userId}, function(err, posts){
+			res.json(posts);
+		});
 	}
 };
